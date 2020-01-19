93d92616dd1d33a85a766205f177dd4b2d3a63f8
Vlad Tkachenko <vlad@fireblink.com>
2020-01-19 13:52:17 +0200

    test: update docker image to use k8s:1.17.0

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM fireblink/kind:latest
+FROM fireblink/kind:1.17.0
 
 # copy static files
 WORKDIR /usr/app
--- test/assets/k8s-init.sh
+++ test/assets/k8s-init.sh
@@ -12,22 +12,21 @@ if [[ $KIND_USE_EXISTING_CLUSTER == 0 ]]; then
 
     echo "-> creating kind cluster..."
     kind create cluster
-   
+
     exit_code=$?
     if [[ $exit_code != 0 ]]; then
         echo "<- failed to create kind cluster"
         exit $exit_code
     fi
-    echo "<- kind cluster created"   
+    echo "<- kind cluster created"
 fi
 
 if [[ -z "$KUBECONFIG" ]]; then
-    # export kube config
-    export KUBECONFIG="$(kind get kubeconfig-path)"
+    kind get kubeconfig > ~/.kube/config
+else
+  # copy kubeconfig into default location for test purposes
+  cp $KUBECONFIG ~/.kube/config
 fi
-
-# copy kubeconfig into default location for test purposes
-cp $KUBECONFIG ~/.kube/config
 
 if [[ -n "$KIND_COPY_KUBECONFIG_TO" ]]; then
     cp $KUBECONFIG $KIND_COPY_KUBECONFIG_TO
